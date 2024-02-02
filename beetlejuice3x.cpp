0d16f5370e9b4ce7a99385676edd5a53dadc274b
TolbyKownsend <Tolbykownsend@gmail.com>
2024-02-02 01:09:27 -0800

    Add files via upload

---
+++ beetlejuice3x.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input;
+    double beatlejuice; 
+    cout << "say beetle juice 3x " << endl;
+    cin >> input;
+    if (input == "beetlejuice3x")
+    {
+          cout << "ITS SHOW TIME " << endl;
+    }
+    
+
+return 0;
+}
